9ae7b5237b80a05f519c9189e038b77be8461d3d
devencoded <71984199+devencoded@users.noreply.github.com>
2020-10-05 11:56:03 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,18 @@
+test1 = int(input("enter the marks"))
+test2 = int(input("enter the marks"))
+test3 = int(input("enter the marks"))
+average = ((test1 + test2 + test3 )/3)
+print(average)
+if average > 75:
+    print("congratulations")
+if average >51 < 70:
+    print("try hard")
+if average >35 < 50:
+    print('pass')
+else:
+     print('fail')
+       
+    
+    
+
+
